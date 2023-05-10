23987f111286779be425d6922819b7917daca834
root <mgiraudo@ies21.edu.ar>
2023-05-10 13:50:28 -0300

    Agregar archivos de proyecto.

---
+++ frmPrincipal.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace pryLunaBaseDeDatos
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnComando = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(202, 61);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(356, 130);
+            this.btnConectar.TabIndex = 0;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(228, 217);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // btnComando
+            // 
+            this.btnComando.Location = new System.Drawing.Point(202, 247);
+            this.btnComando.Name = "btnComando";
+            this.btnComando.Size = new System.Drawing.Size(356, 130);
+            this.btnComando.TabIndex = 2;
+            this.btnComando.Text = "Comando";
+            this.btnComando.UseVisualStyleBackColor = true;
+            this.btnComando.Click += new System.EventHandler(this.btnComando_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnComando);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConectar);
+            this.Name = "frmPrincipal";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmPrincipal_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnComando;
+    }
+}
+
---
+++ frmPrincipal.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.OleDb;
+
+namespace pryLunaBaseDeDatos
+{
+
+
+    public partial class frmPrincipal : Form
+    {
+        OleDbConnection miConexion;
+        OleDbCommand miComando;
+        OleDbDataReader Reader;
+
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+             miConexion = new OleDbConnection();
+             miConexion.ConnectionString = "Provider = Microsoft.Jet.OLEDB.4.0; Data Source = C:\mydatabase.mdb;";
+             miConexion.Open();
+
+             label1.Text = "Conexion Segura";
+             label1.BackColor = Color.Green;
+            }
+           
+            catch(Exception ex)
+            {
+                label1.Text = "Hubo un error: " + ex.Message + ex.Source;
+                label1.BackColor = Color.Red;
+            }
+
+
+
+        }
+
+        private void btnComando_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                miComando = new OleDbCommand();
+                miComando.Connection
+            }
+        }
+    }
+
+}
